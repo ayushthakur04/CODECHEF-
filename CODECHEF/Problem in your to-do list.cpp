3d1b13711f2b89064154c2a89e325163f24c9e20
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-01-18 23:03:54 +0530

    Create Problem in your to-do list.cpp

---
+++ CODECHEF/Problem in your to-do list.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int n;
+	   cin>>n;
+	   int a[n];
+	   for(int i=0;i<n;i++)
+	   {
+	       cin>>a[i];
+	   }
+	   int count=0;
+	   for(int i=0;i<n;i++)
+	   {
+	       if(a[i]>=1000)
+	       {
+	           count++;
+	       }
+	   }
+	   cout<<count<<"\n";
+	}
+	return 0;
+}
