f11173635918857e76ed2e9402e3776a5ed0743d
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-09-14 22:34:44 +0530

    Create 11. Container With Most Water.cpp

---
+++ LEETCODE/11. Container With Most Water.cpp
@@ -0,0 +1,21 @@
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0 , j=height.size()-1;
+        int answer=0;
+        while(i<j)
+        {
+            answer=max(answer,min(height[i],height[j])*(j-i));
+            if (height[i]<height[j]) i++;
+            else j--;
+    
+        }
+        return answer;
+    }
+};
